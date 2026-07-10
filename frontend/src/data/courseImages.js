33ba00774b7d0bc26cf6cb7107e711424397c826
Mahendra79 <janamahendra79@gmail.com>
2026-07-10 11:55:41 +0530

    add imae urls 52,58

--- frontend/src/data/courseImages.js
+++ frontend/src/data/courseImages.js
@@ -2,12 +2,12 @@ export const DEFAULT_COURSE_IMAGE_URL =
   "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/971a437a-f346-4419-ae5a-3f0febd3a494.jpeg";
 
 export const COURSE_IMAGE_URLS_BY_ID = {
-  52: "https://cdn.corenexis.com/files/c/6651645720.png",
+  52: "https://cdn.corenexis.com/f/s2KtMlvceE6.png",
   53: "https://cdn.corenexis.com/files/c/8776583720.png",
   54: "https://cdn.corenexis.com/files/c/4223578720.png",
   55: "https://cdn.corenexis.com/files/c/5482339720.png",
   56: "https://cdn.corenexis.com/f/HNvRe3NCqaf.png",
-  58: "https://cdn.corenexis.com/files/c/5482339720.png",
+  58: "https://cdn.corenexis.com/f/FOg9Q5WiE6H.png",
   60: "https://cdn.corenexis.com/f/BUqXDT5gwSF.png",
   59: "https://cdn.corenexis.com/f/ikPV93Znw8Y.png",
   57: "https://cdn.corenexis.com/f/wRN2WypaqvX.png",
